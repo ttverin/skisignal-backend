b7417e50f7ee7e9a698f5a5ba4d23793bda36213
Teemu Tverin <teemu.tverin@gmail.com>
2026-02-04 11:00:54 +0100

    chore: remove management for workspace id, remove verbose logging

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,6 +64,7 @@ resource "azurerm_application_insights" "ai" {
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
   tags                = local.common_tags
+  workspace_id        = azurerm_log_analytics_workspace.la.id
 }
 
 # -----------------------
