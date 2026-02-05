3c4c337d7dabd45bcce7935f59b1d73f5b5cb329
Teemu Tverin <teemu.tverin@gmail.com>
2026-02-05 19:58:42 +0100

    chore: add best-day function

---
+++ api/best-day/index.js
@@ -0,0 +1,28 @@
+const resorts = require("../shared/resorts");
+const getForecast = require("../shared/forecast");
+const scoreDay = require("../shared/scoring");
+
+module.exports = async function (context) {
+  let results = [];
+
+  for (const resort of Object.keys(resorts)) {
+    const forecast = await getForecast(resort);
+    const scores = scoreDay(forecast);
+
+    results.push({
+      resort,
+      ...forecast,
+      ...scores
+    });
+  }
+
+  results.sort((a, b) => b.snowScore - a.snowScore);
+
+  context.res = {
+    status: 200,
+    body: {
+      best: results[0],
+      all: results
+    }
+  };
+};
