bf1f6850a2561d8f95c73206fdae8495e9e73a6d
Teemu Tverin <teemu.tverin@gmail.com>
2026-02-05 19:46:17 +0100

    feat: make function better

---
+++ api/score-day/index.js
@@ -0,0 +1,22 @@
+const getForecast = require("../shared/forecast");
+const scoreDay = require("../shared/scoring");
+
+module.exports = async function (context, req) {
+  const resort = req.query.resort || "Zermatt";
+
+  try {
+    const forecast = await getForecast(resort);
+    const scores = scoreDay(forecast);
+
+    context.res = {
+      status: 200,
+      body: {
+        resort,
+        ...forecast,
+        ...scores
+      }
+    };
+  } catch (err) {
+    context.res = { status: 500, body: { error: err.message } };
+  }
+};
